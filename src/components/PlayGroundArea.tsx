c2696d54c171585c1328066984bae527893944b8
ozaieee <ozaieee@Mds-MacBook-Pro.local>
2024-09-18 19:43:48 +0530

    build fix

--- src/components/PlayGroundArea.tsx
+++ src/components/PlayGroundArea.tsx
@@ -1,11 +1,11 @@
-import Inconnect from "./Inconnect";
+import { Preconnect } from "./Preconnect";
 
 const PlayGroundArea: React.FC = () => {
   
 
   return (
     <div className="bg-darkBlue min-h-[80vh] flex flex-col items-center justify-center text-center text-white mx-10 mb-10 p-4">
-      <Inconnect/>
+      <Preconnect/>
     </div>
   );
 };
