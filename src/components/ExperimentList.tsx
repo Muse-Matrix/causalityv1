fc4a0f0c85fa4ef239d142c29ceb16c1c1fd73bf
ozaieee <mdozairq@gmail.com>
2024-10-02 23:54:38 +0530

    v30

--- src/components/ExperimentList.tsx
+++ src/components/ExperimentList.tsx
@@ -12,11 +12,8 @@ const ExperimentList: React.FC = () => {
   const {
     museBrainwaves,
     isMuseRecording,
-    isMuseDataRecorded,
-    startMuseRecording,
     stopMuseRecording,
     saveAndDownloadRecordedData,
-    discardMuseRecording,
   } = useExperimentPlayground();
 
   const router = useRouter();
@@ -53,7 +50,7 @@ const ExperimentList: React.FC = () => {
   };
 
   return (
-    <div className="w-full max-w-4xl mx-auto space-y-6 font-mono text-white">
+    <div className="w-full max-w-4xl mx-auto space-y-6 font-mono text-white overflow-y-scroll">
       {selectedExperiment === null ? (
         <>
           <div className="flex items-center space-x-4 mb-8">
@@ -61,7 +58,7 @@ const ExperimentList: React.FC = () => {
               Saved Experiments
             </h1>
           </div>
-          <div className="p-8 space-y-7 text-md w-full max-w-2xl md:max-w-3xl relative">
+          <div className="p-8 space-y-7 text-md w-full max-w-2xl md:max-w-3xl relative overflow-y-scroll">
             <div className="space-y-4">
               {experiments.length > 0 ? (
                 experiments.map((experiment) => (
